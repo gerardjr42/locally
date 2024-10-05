fcfa5540903f236d7ccdd8708f2e88fa32dc5519
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-05 16:58:13 -0400

    changes env filename to env.local

---
+++ src/components/navigation-bar.jsx
@@ -0,0 +1,14 @@
+function NavigationBar() {
+  return (
+    <nav className="navbar bg-base-100 flex flex-row justify-between">
+      <img>
+        <a />
+      </img>
+      <img>
+        <a />
+      </img>
+    </nav>
+  );
+}
+
+export default NavigationBar;
