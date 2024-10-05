34ef4e40679ba849c49b4636d9ba030ff5e5961c
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-05 19:57:20 -0400

    feat: initializes the nav bar with a toggle display for the account menu

---
+++ src/components/navigation-bar.jsx
@@ -0,0 +1,70 @@
+function NavigationBar() {
+  return (
+    <div className="drawer">
+      <input
+        id="account-hamburger-menu-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+      />
+      <div className="drawer-content">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+
+        <img
+          src="public/images/LocallyBrandingAssets-04.png"
+          alt="Locally Logo"
+          className="w-24"
+        />
+        <label
+          htmlFor="account-hamburger-menu-drawer"
+          className="btn btn-primary drawer-button"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="account-hamburger-menu-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
