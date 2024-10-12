5055d54d7e5c4157ac21a6c9d9c0e55e009d87b7
gerardjr42 <ggarciajr12@gmail.com>
2024-10-11 23:35:56 -0400

    Refactor registration page 1. Added particle background component 2. Added registration form component 3. Removed facebook and apple sign in options 4. Added functionality to sign in with Google

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,153 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function RegistrationForm() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [agreeTerms, setAgreeTerms] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email.trim() || !password.trim()) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+
+    if (!agreeTerms) {
+      toast.error("Please agree to the Terms of Service and Privacy Policy");
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      if (error) throw error;
+
+      if (data) {
+        console.log("User signed up successfully:", data);
+        router.push("/register/details");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const handleGoogleSignup = async () => {
+    toast.loading("Signing up with Google...");
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/register/details",
+      },
+    });
+    if (error) {
+      console.error("Error signing up with Google:", error);
+    } else {
+      router.push(data.url);
+    }
+  };
+
+  return (
+    <>
+      <div className="mt-8 space-y-4">
+        <Button
+          variant="outline"
+          className="w-full border-gray-300 hover:bg-gray-50 text-gray-700 flex items-center justify-center"
+          onClick={handleGoogleSignup}
+        >
+          <svg
+            className="w-4 h-4 mr-2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+              fill="#4285F4"
+            />
+            <path
+              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+              fill="#34A853"
+            />
+            <path
+              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+              fill="#FBBC05"
+            />
+            <path
+              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+              fill="#EA4335"
+            />
+          </svg>
+          Sign up with Google
+        </Button>
+      </div>
+
+      <div className="mt-8 text-center">
+        <span className="px-2 bg-white text-gray-500">OR</span>
+        <hr className="border-gray-300 -mt-3" />
+      </div>
+
+      <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="john@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="bg-white border-gray-300 text-gray-900 placeholder-gray-400"
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="•••••••••"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="bg-white border-gray-300 text-gray-900 placeholder-gray-400"
+          />
+        </div>
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            id="terms"
+            checked={agreeTerms}
+            onCheckedChange={setAgreeTerms}
+            required
+            className={agreeTerms ? "" : "border-green-200"}
+          />
+          <label
+            htmlFor="terms"
+            className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${
+              agreeTerms ? "text-[#29A496]" : "text-black"
+            }`}
+          >
+            I agree to the Terms of Service and Privacy Policy
+          </label>
+        </div>
+        <Button
+          type="submit"
+          className="w-full bg-teal-600 hover:bg-teal-700 text-white"
+        >
+          Sign Up
+        </Button>
+      </form>
+    </>
+  );
+}
