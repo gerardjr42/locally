12a34be081cc6f7eeb17d01d195228951296c235
gerardjr42 <ggarciajr12@gmail.com>
2024-10-13 13:03:12 -0400

    PG-287 change redirect route after logging in, to /experiences

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -39,7 +39,7 @@ export default function LoginPage() {
       const { error, data } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/experiences`,
         },
       });
 
