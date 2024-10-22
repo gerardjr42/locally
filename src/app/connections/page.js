73febd4546527aa3c0e7a3d7cd324d827d4c91b8
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-22 17:44:45 -0400

    Add 'Browse Interested Users' option for events without matches
    - Display a message when an event has no matches
    - Add a button to browse interested users for the event
    - Implement navigation to the event's attendees page

--- src/app/connections/page.js
+++ src/app/connections/page.js
@@ -13,70 +13,78 @@ export default function UserMatches() {
   const router = useRouter();
   const { user } = useUserContext();
   const [userEvents, setUserEvents] = useState([]);
- 
 
   useEffect(() => {
     const fetchEvents = async () => {
       const { data: userEventsData, error: userEventsError } = await supabase
-        .from('User_Events')
-        .select('*, Events(*)')
-        .eq('user_id', user.user_id);
+        .from("User_Events")
+        .select("*, Events(*)")
+        .eq("user_id", user.user_id);
 
       if (userEventsError) {
-        console.error('Error fetching events:', userEventsError);
+        console.error("Error fetching events:", userEventsError);
         return;
       }
 
-      const eventsWithMatches = await Promise.all(userEventsData.map(async (event) => {
-        const { data: matchesData, error: matchesError } = await supabase
-          .from('Event_Matches')
-          .select('*')
-          .eq('event_id', event.Events.event_id)
-          .or(`attendee_id.eq.${user.user_id},interest_in_user_id.eq.${user.user_id}`)
-          .eq('mutual_interest', true);
+      const eventsWithMatches = await Promise.all(
+        userEventsData.map(async (event) => {
+          const { data: matchesData, error: matchesError } = await supabase
+            .from("Event_Matches")
+            .select("*")
+            .eq("event_id", event.Events.event_id)
+            .or(
+              `attendee_id.eq.${user.user_id},interest_in_user_id.eq.${user.user_id}`
+            )
+            .eq("mutual_interest", true);
 
-        if (matchesError) {
-          console.error('Error fetching matches:', matchesError);
-          return event;
-        }
-
-        const matches = await Promise.all(matchesData.map(async (match) => {
-          const otherUserId = match.attendee_id === user.user_id ? match.interest_in_user_id : match.attendee_id;
-          const { data: userData, error: userError } = await supabase
-            .from('Users')
-            .select('user_id, first_name, last_name, photo_url')
-            .eq('user_id', otherUserId)
-            .single();
-
-          if (userError) {
-            console.error('Error fetching user data:', userError);
-            return null;
+          if (matchesError) {
+            console.error("Error fetching matches:", matchesError);
+            return event;
           }
 
-          return {
-            match_id: match.match_id,
-            other_user_id: userData.user_id,
-            other_user_photo_url: userData.photo_url,
-            other_user_first_name: userData.first_name,
-            other_user_last_name: userData.last_name,
-            is_mutual: match.mutual_interest,
-            is_confirmed: match.confirmed_together
-          };
-        }));
+          const matches = await Promise.all(
+            matchesData.map(async (match) => {
+              const otherUserId =
+                match.attendee_id === user.user_id
+                  ? match.interest_in_user_id
+                  : match.attendee_id;
+              const { data: userData, error: userError } = await supabase
+                .from("Users")
+                .select("user_id, first_name, last_name, photo_url")
+                .eq("user_id", otherUserId)
+                .single();
 
-        return {
-          ...event,
-          matches: matches.filter(Boolean)
-        };
-      }));
+              if (userError) {
+                console.error("Error fetching user data:", userError);
+                return null;
+              }
+
+              return {
+                match_id: match.match_id,
+                other_user_id: userData.user_id,
+                other_user_photo_url: userData.photo_url,
+                other_user_first_name: userData.first_name,
+                other_user_last_name: userData.last_name,
+                is_mutual: match.mutual_interest,
+                is_confirmed: match.confirmed_together,
+              };
+            })
+          );
+
+          return {
+            ...event,
+            matches: matches.filter(Boolean),
+          };
+        })
+      );
 
       setUserEvents(eventsWithMatches);
-    };  
+    };
     if (user) {
       fetchEvents();
     }
   }, [user, supabase]);
-  
+
   console.log(userEvents);
 
   return (
@@ -106,46 +114,74 @@ export default function UserMatches() {
 
       <div className="w-full p-4 md:flex-row md:flex md:flex-wrap md:justify-between lg:px-8">
         {userEvents.map((event) => (
-          <div key={event.Events.event_id} className="collapse collapse-arrow bg-gray-100 pb-1 mb-4">
-            <input type="checkbox" defaultChecked={true}/>
+          <div
+            key={event.Events.event_id}
+            className="collapse collapse-arrow bg-gray-100 pb-1 mb-4"
+          >
+            <input type="checkbox" defaultChecked={true} />
             <div className="collapse-title text-sm text-gray-500 font-semibold">
               <p>{event.Events.event_name}</p>
-              <p className="text-xs text-gray-400">{formatDate(event.Events.event_time)}</p>
+              <p className="text-xs text-gray-400">
+                {formatDate(event.Events.event_time)}
+              </p>
             </div>
             <div className="collapse-content flex flex-row items-center justify-evenly">
-              {event.matches.map((match) => (
-                <div key={match.match_id} className="flex-shrink-0 w-20">
-                <div className="relative mb-1">
-                  <div 
-                    className="rounded-full overflow-hidden w-20 h-20 cursor-pointer"
-                    onClick={() => router.push(`/connections/${match.match_id}`)}
-                  >
-                    <Image
-                      src={match.other_user_photo_url}
-                      alt={`${match.other_user_first_name} ${match.other_user_last_name[0]}.`}
-                      width={80}
-                      height={80}
-                      className="object-cover"
-                    />
+              {event.matches && event.matches.length > 0 ? (
+                event.matches.map((match) => (
+                  <div key={match.match_id} className="flex-shrink-0 w-20">
+                    <div className="relative mb-1">
+                      <div
+                        className="rounded-full overflow-hidden w-20 h-20 cursor-pointer"
+                        onClick={() =>
+                          router.push(`/connections/${match.match_id}`)
+                        }
+                      >
+                        <Image
+                          src={match.other_user_photo_url}
+                          alt={`${match.other_user_first_name} ${match.other_user_last_name[0]}.`}
+                          width={80}
+                          height={80}
+                          className="object-cover"
+                        />
+                      </div>
+                      {match.is_confirmed && (
+                        <span className="absolute top-0 right-0 bg-teal-500 text-white text-xs px-1 py-0.5 rounded-full text-[10px]">
+                          Confirmed
+                        </span>
+                      )}
+
+                      {!match.is_confirmed && match.is_mutual && (
+                        <span className="absolute top-0 right-0 bg-teal-500 text-white text-xs px-1 py-0.5 rounded-full text-[10px]">
+                          Matched
+                        </span>
+                      )}
+                    </div>
+                    <p className="text-center text-xs">
+                      {match.other_user_first_name}{" "}
+                      {match.other_user_last_name[0]}.
+                    </p>
                   </div>
-                  {match.is_confirmed && (
-                    <span className="absolute top-0 right-0 bg-teal-500 text-white text-xs px-1 py-0.5 rounded-full text-[10px]">
-                      Confirmed
-                    </span>
-                  )}
-                  
-                  {!match.is_confirmed && match.is_mutual && (
-                    <span className="absolute top-0 right-0 bg-teal-500 text-white text-xs px-1 py-0.5 rounded-full text-[10px]">
-                      Matched
-                    </span>
-                  )}
-                  
+                ))
+              ) : (
+                <div className="flex flex-col items-center justify-center py-3">
+                  <p className="text-center text-sm font-semibold text-teal-600">
+                    No matches yet? No worries!
+                  </p>
+                  <p className="text-xs text-teal-600 text-center">
+                    Check out other interested users who might be your match!
+                  </p>
+                  <button
+                    className="w-3/4 bg-teal-500 text-white p-2 my-2 rounded-full font-semibold flex items-center justify-center"
+                    onClick={() =>
+                      router.push(
+                        `/experiences/${event.Events.event_id}/attendees`
+                      )
+                    }
+                  >
+                  Browse Interested Users
+                  </button>
                 </div>
-                <p className="text-center text-xs">
-                  {match.other_user_first_name} {match.other_user_last_name[0]}.
-                </p>
-              </div>
-              ))}
+              )}
             </div>
           </div>
         ))}
