52295a5a1b498c42f7aab3ad61a14959ae41209c
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-23 18:27:33 -0400

    Enhance layout and styling for connection details page
    - Implement responsive flex layout for better mobile experience
    - Add card components for user match and event information
    - Integrate user profile images with fist bump icon
    - Style buttons and text for improved readability and user interaction
    - Implement collapsible section for connection confirmation

--- src/app/connections/[connectionId]/page.js
+++ src/app/connections/[connectionId]/page.js
@@ -48,14 +48,13 @@ export default function UserMatches() {
     fetchEventInfo();
   }, [params.connectionId]);
 
-
   const fetchMatchAndUserInfo = async () => {
     const { data: matchData, error: matchError } = await supabase
       .from("Event_Matches")
       .select("*")
       .eq("match_id", params.connectionId)
       .single();
-  
+
     if (matchError) {
       console.error("Error fetching match data:", matchError);
       return;
@@ -63,25 +62,26 @@ export default function UserMatches() {
 
     setMatchData(matchData);
 
-    const otherUserId = matchData.attendee_id === user.user_id 
-      ? matchData.interest_in_user_id 
-      : matchData.attendee_id;
-  
+    const otherUserId =
+      matchData.attendee_id === user.user_id
+        ? matchData.interest_in_user_id
+        : matchData.attendee_id;
+
     const { data: userData, error: userError } = await supabase
       .from("Users")
       .select("*")
       .eq("user_id", otherUserId)
       .single();
-  
+
     if (userError) {
       console.error("Error fetching user data:", userError);
     } else {
       setOtherUser(userData);
     }
-  
+
     fetchEventInfo();
   };
-  
+
   useEffect(() => {
     if (user && params.connectionId) {
       fetchMatchAndUserInfo();
@@ -94,38 +94,79 @@ export default function UserMatches() {
     router.push(`/experiences/${eventInfo.event_id}`);
   }
 
-
-
   return (
     <div className="flex flex-col items-center justify-center">
       <NavigationBar />
 
-      <div className="w-full p-4">
+      <div className="w-full p-2">
+        <div className="card flex items-center justify-center bg-base-100 shadow-sm mb-4 p-2">
+          <div className="card-body flex-col items-center justify-center p-2">
+            <h2 className="card-title text-gray-600">{`You've Matched with ${otherUser?.first_name}!`}</h2>
+            <div className="card-image w-full h-full overflow-hidden flex flex-row items-center justify-evenly px-1 py-2">
+            <Image
+              src={user?.photo_url}
+              alt={user?.first_name}
+              width={120}
+              height={120}
+              className= "ring-teal-500 ring-primary ring-offset-base-100 rounded-full ring ring-offset-2 object-cover "
+            />
+            <Image
+            src="/images/fistbump.svg"
+            alt="Fist Bump"
+            width={50}
+            height={50}
+            className="mx-2"
+            />
+            <Image
+              src={otherUser?.photo_url}
+              alt={otherUser?.first_name}
+              width={120}
+              height={120}
+              className= "ring-teal-500 ring-primary ring-offset-base-100 rounded-full ring ring-offset-2 object-cover"
+            />
+          </div>
+            <p className="text-xs text-center text-gray-500">{`Time to introduce yourselves, coordinate your meetup, and confirm your connection!`}</p>
+            <button className="w-1/2 bg-teal-500 text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center">
+              Chat
+            </button>
+          </div>
+          
+        </div>
+
         {eventInfo && (
-            <div className="card bg-base-100 image-full w-full shadow-xl my-4">
-            <figure>
-              <img
+          <div className="card bg-base-100 image-full w-full shadow-sm my-4">
+            <div className="card-image relative w-full h-full overflow-hidden rounded-xl">
+              <Image
                 src={eventInfo.event_image_url}
-                alt={eventInfo.event_name} />
-            </figure>
+                alt={eventInfo.event_name}
+                fill={true}
+                className="object-cover"
+              />
+            </div>
             <div className="card-body">
               <h2 className="card-title m">{eventInfo.event_name}</h2>
               <p className="text-xs">{formatDate(eventInfo.event_time)}</p>
               <p className="text-xs">{eventInfo.event_details}</p>
               <div className="card-actions justify-center">
-                <button className="w-1/2 bg-teal-500 text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center" onClick={handleViewEvent}>View Experience</button>
+                <button
+                  className="w-3/4 outline text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center"
+                  onClick={handleViewEvent}
+                >
+                  View Experience
+                </button>
               </div>
             </div>
           </div>
         )}
-        <div className="collapse collapse-arrow bg-gray-100 pb-1 mb-4">
+
+        <div className="collapse collapse-arrow bg-gray-100 pb-1 mb-4 shadow-sm">
           <input type="checkbox" defaultChecked={true} />
           <div className="collapse-title">
             <p className="text-sm text-teal-700 font-semibold">
               Confirm Your Connection
             </p>
             <p className="text-xs text-gray-500">
-              Will you be connecting with User at your experience?
+              {`Will you be connecting with ${otherUser?.first_name} at the ${eventInfo?.event_name}?`}
             </p>
           </div>
           <div className="collapse-content flex flex-row items-center justify-evenly">
