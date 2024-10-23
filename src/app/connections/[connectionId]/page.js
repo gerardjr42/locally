44ba00f2ac9f1475f6c24d7d47b3f6755bd0cec2
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-22 20:19:33 -0400

    Adds an attendance confirmation button for the user to confirm that they will be attending their experience with the user they matched with

--- src/app/connections/[connectionId]/page.js
+++ src/app/connections/[connectionId]/page.js
@@ -15,8 +15,23 @@ export default function UserMatches() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-        <NavigationBar />
-    </div>
-  )
+      <NavigationBar />
 
+      <div className="w-full p-4">
+        <div className="collapse collapse-arrow bg-gray-100 pb-1 mb-4">
+          <input type="checkbox" defaultChecked={true} />
+          <div className="collapse-title">
+            <p className="text-sm text-teal-700 font-semibold">
+              Confirm Your Connection
+            </p>
+            <p className="text-xs text-gray-500">Will you be attending your experience with User?</p>
+          </div>
+          <div className="collapse-content flex flex-row items-center justify-evenly">
+            <button className="w-1/2 bg-teal-500 text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center">
+            Confirm</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
