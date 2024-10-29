265aef13efe0a717356edb18019475cf9078f8c4
lynnjaco <jacobs.lynnette@gmail.com>
2024-10-29 19:47:25 -0400

    feat: Add conditional rendering for confirmed users
    - Implement conditional return based on areUsersConfirmed state
    - Add new component layout for confirmed user state
    - Enhance user experience by showing different content for confirmed matches

--- src/app/connections/[connectionId]/page.js
+++ src/app/connections/[connectionId]/page.js
@@ -16,6 +16,8 @@ export default function UserMatches() {
   const [eventInfo, setEventInfo] = useState(null);
   const [matchData, setMatchData] = useState(null);
   const [otherUser, setOtherUser] = useState(null);
+  const [areUsersConfirmed, setAreUsersConfirmed] = useState(false);
+  const [didConnectionOccur, setDidConnectionOccur] = useState(false);
 
   const fetchEventInfo = async () => {
     const { data: matchData, error: matchError } = await supabase
@@ -82,16 +84,138 @@ export default function UserMatches() {
     fetchEventInfo();
   };
 
+  function handleViewEvent() {
+    router.push(`/experiences/${eventInfo.event_id}`);
+  }
+
+  const checkConnectionOccurred = async () => {
+    const { data: matchData, error: matchError } = await supabase
+      .from("Event_Matches")
+      .select("*, Events(event_time)")
+      .eq("match_id", params.connectionId)
+      .single();
+  
+    if (matchError) {
+      console.error("Error fetching match data:", matchError);
+      return;
+    }
+  
+    if (matchData && matchData.confirmed_together) {
+      const eventTime = new Date(matchData.Events.event_time);
+      const currentTime = new Date();
+      setAreUsersConfirmed(true);
+
+      if (currentTime > eventTime) {
+        setDidConnectionOccur(true);
+      }
+    }
+  };
+  
+
   useEffect(() => {
     if (user && params.connectionId) {
       fetchMatchAndUserInfo();
+      checkConnectionOccurred();
     }
   }, [params.connectionId, user]);
 
-  console.log(otherUser);
+  if (areUsersConfirmed) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+      <NavigationBar />
 
-  function handleViewEvent() {
-    router.push(`/experiences/${eventInfo.event_id}`);
+      <div className="w-full p-2">
+        <div className="card flex items-center justify-center bg-base-100 shadow-sm mb-4 p-2">
+          <div className="card-body flex-col items-center justify-center p-2">
+            <h2 className="card-title text-gray-600">{`You'll be Connecting with ${otherUser?.first_name}!`}</h2>
+            <div className="card-image w-full h-full overflow-hidden flex flex-row items-center justify-evenly px-1 py-5">
+              <div className="avatar w-3/4">
+                <div className="ring-primary ring-teal-500 ring-offset-base-100 w-full rounded-full ring ring-offset-2">
+                  <img src={user?.photo_url} />
+                </div>
+              </div>
+              <Image
+                src="/images/fistbump.svg"
+                alt="Fist Bump"
+                width={50}
+                height={50}
+                className="mx-2"
+              />
+              <div className="avatar w-3/4">
+                <div className="ring-primary ring-teal-500 ring-offset-base-100 w-full rounded-full ring ring-offset-2">
+                  <img src={otherUser?.photo_url} />
+                </div>
+              </div>
+            </div>
+            <p className="text-xs text-center text-gray-500">{`Congrats! We hope your connection goes well and you enjoy yourselves at ${eventInfo.event_name}!`}</p>
+            <button className="w-1/2 bg-teal-500 text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center">
+              Add To Calendar
+            </button>
+          </div>
+        </div>
+
+        {eventInfo && (
+          <div className="card bg-base-100 image-full w-full shadow-sm my-4">
+            <div className="card-image relative w-full h-full overflow-hidden rounded-xl">
+              <Image
+                src={eventInfo.event_image_url}
+                alt={eventInfo.event_name}
+                fill={true}
+                className="object-cover"
+              />
+            </div>
+            <div className="card-body">
+              <h2 className="card-title m">{eventInfo.event_name}</h2>
+              <p className="text-xs">{formatDate(eventInfo.event_time)}</p>
+              <p className="text-xs">{eventInfo.event_details}</p>
+              <div className="card-actions justify-center">
+                <button
+                  className="w-3/4 outline text-white text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center"
+                  onClick={handleViewEvent}
+                >
+                  View Experience
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        <div className="collapse collapse-arrow bg-gray-100 pb-1 mb-4 shadow-sm">
+          <input type="checkbox"/>
+          <div className="collapse-title">
+            <p className="text-sm text-teal-700 font-semibold">
+              Your Connection Is Confirmed!
+            </p>
+            <p className="text-xs text-gray-500">
+              {`You ahd ${otherUser?.first_name} are confirmed to connect at the ${eventInfo?.event_name}?`}
+            </p>
+          </div>
+          <div className="collapse-content flex flex-row items-center justify-evenly">
+            <button className="w-1/2 bg-transparent text-gray-400 outline text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center">
+              Cancel Connection
+            </button>
+          </div>
+        </div>
+
+        {/* <div className="collapse collapse-arrow bg-gray-100 pb-1 mb-4 shadow-sm">
+          <input type="checkbox" />
+          <div className="collapse-title">
+            <p className="text-sm text-gray-400 font-semibold">
+              Cancel This Match
+            </p>
+          </div>
+          <div className="collapse-content flex flex-col items-center justify-evenly">
+            <p className="text-xs text-gray-400">
+              {`Are you sure you want to cancel your match with ${otherUser?.first_name} for the ${eventInfo?.event_name}?`}
+            </p>
+            <button className="w-1/2 outline text-gray-400 text-sm p-4 my-2 rounded-full font-semibold flex items-center justify-center">
+              Cancel
+            </button>
+          </div>
+        </div> */}
+      </div>
+    </div>
+    )
   }
 
   return (
@@ -191,3 +315,5 @@ export default function UserMatches() {
     </div>
   );
 }
+
+
