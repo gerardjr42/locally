c5ce0e2ed720ccab356da97c3c0c093766c0a2f5
gerardjr42 <ggarciajr12@gmail.com>
2024-10-28 23:48:41 -0400

    make chat full screen height

--- src/app/connections/chats/[chatId]/page.js
+++ src/app/connections/chats/[chatId]/page.js
@@ -50,46 +50,41 @@ export default function ChatPage() {
       );
       console.log("connectedUser", connectedUser);
 
-      const channel = client.channel("messaging", "spacejelly", {
+      const channel = client.channel("messaging", "matching-channel", {
         //we can make this unique with only two users
         /**
          * members: ['vishal', 'neil'],
          * name: 'Awesome channel about traveling'
          */
-        name: "Space Jelly",
+        name: "Matching Channel",
       });
       setChannel(channel);
     })();
 
-    // return () => {
-    //   if (client) {
-    //     client.disconnectUser();
-    //     setClient(null);
-    //     setChannel(null);
-    //   }
-    // };
+    return () => {
+      if (client) {
+        client.disconnectUser();
+        setClient(null);
+        setChannel(null);
+      }
+    };
   }, [user]);
-
-  const filters = { members: { $in: [user?.user_id] }, type: "messaging" };
-  const options = { presence: true, state: true };
-  const sort = { last_message_at: -1 };
 
   return (
     <>
       {client && channel && (
-        <Chat client={client}>
-          {/* We use ChannelList to list all the channels the user is part of, we have to use this logic in the chat's page not [chatId].**/}
-          {/* !Add this logic later */}
-          {/* <ChannelList sort={sort} filters={filters} options={options} /> */}
-          <Channel channel={channel}>
-            <Window>
-              <ChannelHeader />
-              <MessageList />
-              <MessageInput />
-            </Window>
-            <Thread />
-          </Channel>
-        </Chat>
+        <div className="h-screen w-full">
+          <Chat client={client} theme="str-chat__theme-light">
+            <Channel channel={channel}>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput focus className="" />
+              </Window>
+              <Thread />
+            </Channel>
+          </Chat>
+        </div>
       )}
     </>
   );
